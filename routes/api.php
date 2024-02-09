6369373a01b9f5e319c56b95642475462d29212b
japheth <japhethoz@gmail.com>
2024-02-09 16:23:04 -0600

    create v2

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V1\PostController as PostV2;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::apiResource('v1/posts',PostV1::class)
+    ->only(['index', 'show', 'destroy']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+    Route::apiResource('v2/posts',PostV2::class)
+    ->only(['index', 'show', 'destroy']);
